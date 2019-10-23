a6aa21770650a3b7a2cf0c12df075c2e10ac3e92
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-23 02:38:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,8 +6,11 @@ class PigLatinizer
   def piglatinize(user_input)
     input_array = user_input.split(" ")
     
+    new_array = []
     input_array.each do |word|
       if word[0].scan(/[aeoui]/)
+        new_array << word + 'way'
+      end 
     end 
   end 
   
